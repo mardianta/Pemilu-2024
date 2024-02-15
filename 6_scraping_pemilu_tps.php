2ccb297ab1ab55d3f991de30d3fbedd1ac99bec8
Mardianta PA <38100120+mardianta@users.noreply.github.com>
2024-02-15 09:43:23 +0700

    update tps

--- 6_scraping_pemilu_tps.php
+++ 6_scraping_pemilu_tps.php
@@ -3,7 +3,9 @@
 // $json_url = 'https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/0.json';
 $kode_prov = 11;
 $kode_kab = 1101;
-$json_url = 'https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/'.$kode_prov.'/'.$kode_kab.'.json';
+$kode_kec = 110101;
+$kode_kel = 1101012001;
+$json_url = 'https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/'.$kode_prov.'/'.$kode_kab.'/'.$kode_kec.'/'.$kode_kel.'.json';
 
 // Mengambil data dari URL JSON
 $json_data = file_get_contents($json_url);
@@ -15,27 +17,27 @@ $data = json_decode($json_data, true);
 if ($data === null) {
     echo "Gagal mendapatkan atau mengurai data JSON.";
 } else {
-    foreach ($data as $kec) {
+    foreach ($data as $tps) {
         // Membuat folder berdasarkan nama kode
-        $folder_path_kec = "data_json/".$kode_prov.'/'.$kode_kab.'/'.$kec['kode'];
-        if (!is_dir($folder_path_kec)) {
-            mkdir($folder_path_kec);
+        $folder_path_tps = "data_json/".$kode_prov.'/'.$kode_kab.'/'.$kode_kec.'/'.$kode_kel.'/'.$tps['kode'];
+        if (!is_dir($folder_path_tps)) {
+            mkdir($folder_path_tps);
         }
 
-        $json_url= 'https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/'.$kode_prov.'/'.$kode_kab.'/'.$kec['kode'].'.json';
+        $json_url= 'https://sirekap-obj-data.kpu.go.id/pemilu/hhcw/ppwp/'.$kode_prov.'/'.$kode_kab.'/'.$kode_kec.'/'.$kode_kel.'/'.$tps['kode'].'.json';
         $json_data = file_get_contents($json_url);
         $json_data_string = json_encode($json_data, JSON_PRETTY_PRINT);
 
-        $file_path_kec = $folder_path_kec.'/ppwp_0_'.$kode_prov.'_'.$kode_kab.'_'.$kec['kode'].'.json';
+        $file_path_tps = $folder_path_tps.'/ppwp_0_'.$kode_prov.'_'.$kode_kab.'_'.$kode_kec.'_'.$kode_kel.'_'.$tps['kode'].'.json';
     
         // Menyimpan data ke dalam file
-        if (file_put_contents($file_path_kec, $json_data_string)) {
+        if (file_put_contents($file_path_tps, $json_data_string)) {
             echo "Data telah disimpan ke dalam file JSON.";
             // var_dump($file_path_prov);
             // end();
         } else {
             echo "Gagal menyimpan data ke dalam file JSON.";
-            var_dump($file_path_kec);
+            var_dump($file_path_tps);
             end();
         }
     }
